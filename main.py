e2509c872d974b3fae0c6776d7655019d02e0a69
Tugroll <tgrlarmn5@gmail.com>
2024-12-25 17:37:16 +0300

    Updated Boostrap

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from flask import Flask, render_template
 import requests
-from post import Post
 
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", blog_data= post_objects)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
-@app.route('/post/<int:index>')
-def go_to_post(index):
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if index == blog_post.id:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
 
-
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
